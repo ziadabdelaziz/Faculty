69f1ae8f33fb2dbecbc1ef667313856465c02063
ziad <ziadabdelaziz1995@gmail.com>
2024-03-18 16:28:49 +0200

    [ADD]: task1

--- task1.py
+++ task1.py
@@ -1,10 +1,9 @@
-import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
-
-nltk.download()
 
 example_text = 'Hello mr. Smith, how are you doing today? The weather is greate and python is Awesome, The sky is pinkish-blue. You should not eat card-board'
 
 print(sent_tokenize(example_text))
 
 print(word_tokenize(example_text))
+
+print(example_text.split())
